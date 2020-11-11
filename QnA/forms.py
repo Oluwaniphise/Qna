115ef285cb3adcbc9eb6884893ca0b589203676c
Enoch Oduyale <oduyaleenoch@gmail.com>
2020-11-11 02:41:14 -0800

    fixed form issues

--- QnA/forms.py
+++ QnA/forms.py
@@ -8,7 +8,7 @@ class QuestionForm(ModelForm):
 
     class Meta:
         model = Question
-        fields = [  'title', 'details' ]
+        fields = [  'user','title', 'details' ]
      
     title = forms.CharField( label='Question_title',  widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Question title'}))
     details = forms.CharField(label="Details",  widget=forms.Textarea(attrs={'class':'form-control', 'cols':20, 'rows':5, 'placeholder':'Talk more about your question...'}))
